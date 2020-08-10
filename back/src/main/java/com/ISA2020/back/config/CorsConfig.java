a99dce16a8a83de4e889141501db86b13a5df2b1
Milica Visekruna <MVisekrunaa1909@gmail.com>
2020-08-10 10:41:59 +0200

    CorsConfig.java

--- back/src/main/java/com/ISA2020/back/config/CorsConfig.java
+++ back/src/main/java/com/ISA2020/back/config/CorsConfig.java
@@ -1,31 +1,55 @@
 package com.ISA2020.back.config;
 
-import org.springframework.boot.autoconfigure.security.servlet.WebSecurityEnablerConfiguration;
+
+import static org.springframework.http.HttpMethod.DELETE;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.OPTIONS;
+import static org.springframework.http.HttpMethod.PATCH;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpMethod.PUT;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableWebMvc
 @Configuration
-public class CorsConfig extends WebSecurityEnablerConfiguration {
-
+@Profile("development")
+public class CorsConfig implements WebMvcConfigurer {
+		
 	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-					.allowedMethods("GET", "POST", "PUT", "DELETE")
-					.allowedHeaders("*")
-					.allowedOrigins("http://localhost:4200");
-			}
-		};
+	public CorsFilter corsFilter() {
+		final CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod(GET);
+		config.addAllowedMethod(PUT);
+		config.addAllowedMethod(POST);
+		config.addAllowedMethod(PATCH);
+		config.addAllowedMethod(DELETE);
+		config.addAllowedMethod(OPTIONS);
+		config.setMaxAge((long) 3600000);
+		
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	 }	
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("*").allowedOrigins("http://localhost:4200");
 	}
-	
- 
-	
-	
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/webjars/**")
+		.addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+
 }
