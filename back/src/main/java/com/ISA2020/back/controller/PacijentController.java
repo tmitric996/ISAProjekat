78fe43406abb20f877e3c2260efbb7d61d7f549f
tmitric996 <44475038+tmitric996@users.noreply.github.com>
2020-08-24 16:09:03 +0200

    Add files via upload

--- back/src/main/java/com/ISA2020/back/controller/PacijentController.java
+++ back/src/main/java/com/ISA2020/back/controller/PacijentController.java
@@ -1,26 +1,34 @@
 package com.ISA2020.back.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ISA2020.back.model.Klinika;
-import com.ISA2020.back.service.KlinikaServiceImpl;
+import com.ISA2020.back.model.Pacijent;
+import com.ISA2020.back.service.PacijentServiceImpl;
 
 @RestController
 @EnableAutoConfiguration
-@RequestMapping("/klinika")
-public class KlinikaController {
-
+@RequestMapping(value = "/pacijenti")
+public class PacijentController {
 	
 	@Autowired
-	KlinikaServiceImpl klinikaService;
+	PacijentServiceImpl pacijentService;
 	
-	@PostMapping("/save")
-	public Klinika saveKlinika(@RequestBody Klinika k) {
-		return klinikaService.saveKlinika(k); 
+	@PostMapping
+	public Pacijent save(@RequestBody Pacijent p) {
+		return pacijentService.save(p);
 	}
+	
+	@GetMapping
+	public List<Pacijent> findAll(){
+		return pacijentService.findAll();
+		}
+
 }
